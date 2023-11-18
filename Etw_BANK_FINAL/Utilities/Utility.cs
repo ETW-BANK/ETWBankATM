16a1270976eb6f9016bfcee89edc44b3e67a6957
dinozoff1981 <tensaeg@yahoo.com>
2023-11-18 18:42:14 +0100

    project sceleton completed

---
+++ Etw_BANK_FINAL/Utilities/Utility.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etw_BANK_FINAL.Utilities
+{
+    internal class Utility
+    {
+        // Utility class containing various utility methods
+
+        // Method to animate loading dots
+        public static void AnimateLoadingDots(int numberOfDots, int animationSpeed)
+        {
+            // Code to animate loading dots and control cursor visibility
+
+            Console.CursorVisible = false;
+
+            for (int i = 0; i < numberOfDots; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(animationSpeed);
+            }
+
+            Console.CursorVisible = true;
+        }
+
+        // Method to display a loading message while animating dots
+        public static void Loading()
+        {
+
+            Console.Write("\t\t\t\t\t \u001b[33m Loading \u001b[0m...");
+
+            // Code to display loading message and call the AnimateLoadingDots method
+
+            int numberOfDots = 20;
+            int animationSpeed = 200;
+
+            AnimateLoadingDots(numberOfDots, animationSpeed);
+
+        }
+
+        // Method to hide user input for PIN entry
+        public static SecureString PinHide()
+        {
+            // Code to hide PIN entry as asterisks
+
+            Console.WriteLine("\n\n\t\t\t\t\t\t  \u001b[33m Enter Your PIN\u001b[0m\n");
+            SecureString Pin = new SecureString();
+            ConsoleKeyInfo keyInfo;
+            Console.Write("\n\n\t\t\t\t\t\t\t");
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.Backspace && Pin.Length > 0)
+
+                {
+                    Pin.RemoveAt(Pin.Length - 1);
+                    Console.Write("\b \b");
+                }
+
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Pin.AppendChar(keyInfo.KeyChar);
+                    Console.Write("*");
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Enter);
+
+            {
+                return Pin;
+            }
+        }
+
+        // Method to generate a random account number
+        public static int GenerateAccNumber()
+
+        {
+            // Code to generate a random account number of a specified size
+            Random random = new Random();
+            int Accnumber = 0;
+
+
+            int size = 6;
+            for (int i = 0; i < size; i++)
+            {
+                int rand = random.Next(0, 9);
+                Accnumber = Accnumber * 10 + rand;
+            }
+
+            return Accnumber;
+        }
+        // Method to generate a random PIN
+        public static string GeneratePin()
+
+        {
+            // Code to generate a random PIN of a specified size
+            Random random = new Random();
+            string pinnumber = " ";
+
+            int size = 4;
+
+            for (int i = 0; i < size; i++)
+            {
+                int rand = random.Next(1000, 9999);
+
+                pinnumber = rand.ToString();
+
+
+            }
+
+            return pinnumber;
+        }
+
+
+        // Method to prompt user for currency input and validate
+        public static string CheckCurrency()
+        {
+            string CurrencyType;
+
+            do
+            {
+                // Code to prompt for and validate currency input
+                Console.Write("Enter Currency: ");
+                CurrencyType = Console.ReadLine().ToUpper();
+
+                if (CurrencyType != "USD" && CurrencyType != "EURO" && CurrencyType != "SEK")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n");
+                    Console.WriteLine("\nInvalid Currency please enter");
+                    Console.WriteLine("USD/EURO/SEK");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    return CurrencyType;
+                }
+
+            } while (true);
+        }
+
+        // Method to perform currency conversion
+
+        public static decimal CurrencyChanger(string val1, string val2, decimal amount)
+        {
+            // Code to perform currency conversion based on input values
+
+            decimal exchangeRate = 1.0m; // Default exchange rate if no conversion is needed
+
+            if (val1 == "USD" && val2 == "SEK")
+            {
+                exchangeRate = exchangeRate * 10;
+            }
+            else if (val1 == "SEK" && val2 == "USD")
+            {
+                exchangeRate = exchangeRate / 10;
+            }
+            else if (val1 == "USD" && val2 == "EURO")
+            {
+                exchangeRate = exchangeRate / 1.2m;
+            }
+            else if (val1 == "EURO" && val2 == "USD")
+            {
+                exchangeRate = exchangeRate * 1.2m;
+            }
+
+            // Perform the currency conversion
+            return amount * exchangeRate;
+        }
+
+
+        public static void EscapeKeyCall()
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\t\t\u001b[0m Press \u001b[34m ESC \u001b[0m to exit");
+
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey().Key;
+
+                if (key != ConsoleKey.Escape)
+                {
+                    Console.WriteLine("\n\t\t\t\u001b[31m Wrong key pressed. Press \u001b[34m ESC\u001b[0m \u001b[31m to exit.\t\t\t\u001b[0m");
+                }
+            } while (key != ConsoleKey.Escape);
+        }
+
+
+    }
+}
---
+++ Etw_BANK_FINAL/WelcomeUI/Welcome.cs
@@ -0,0 +1,79 @@
+﻿using Etw_BANK_FINAL.Logins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etw_BANK_FINAL.WelcomeUI
+{
+    internal class Welcome
+    {
+
+        // Method to display the welcome screen and navigate to different login options
+        public static void WelcomeUI()
+        {
+            // Displaying the welcome message and instructions
+            Console.WriteLine($"\n\n\t\t\t  =====================WELCOME TO ETW - BANK========================\n\n");
+            Console.WriteLine("\n\t\t\t\tPlease use \u001b[34m UP \u001b[0m arrow To move up and \u001b[34m Down \u001b[0m to Move Down\n\n");
+
+            // Initializing variables for navigation
+            ConsoleKeyInfo key;
+            int choice = 1;
+            bool isChoosen = false;
+            (int left, int top) = Console.GetCursorPosition();
+
+            string color = "\u001b[32m";
+
+            // Loop to navigate through options until choice is made
+            while (!isChoosen)
+            {
+                Console.SetCursorPosition(left, top);
+
+                // Displaying administrator and customer options
+                Console.WriteLine($"\n\t\t\t\t\t\t   {(choice == 1 ? color : "")}Administrator\u001b[0m\n");
+                Console.WriteLine($"\n\t\t\t\t\t\t    {(choice == 2 ? color : "")}Customer\u001b[0m\n");
+                Console.WriteLine($"\n\n\t\t\t\t\t     Hit \u001b[32m ENTER \u001b[0m to Continue  \n");
+
+                key = Console.ReadKey(true);
+
+                // Handling arrow key input for navigation
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (choice == 2)
+                            choice = 1;
+                        else
+                            choice++;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (choice == 1)
+                            choice = 2;
+                        else
+                            choice--;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        Console.WriteLine("\t\t\n");
+                        Utility.Loading();
+                        isChoosen = true;
+
+                        // Navigating based on the selected choice
+                        if (choice == 1)
+                        {
+                            Console.Clear();
+                            Login.AdminLogin();
+                        }
+                        else if (choice == 2)
+                        {
+                            Console.Clear();
+                            Login.UserLogin();
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+}
